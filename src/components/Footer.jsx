d06420da25da5caaa81b94a1eee458641c724ab3
Bhavya <121705262+bhavya681@users.noreply.github.com>
2024-02-01 15:07:02 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,7 +27,7 @@ const Footer = () => {
 
         <VStack>
           <Avatar  boxSize={"28"} mt={["4", "0"]} src={avatarSrc} />
-          <Text>Our Founder</Text>
+          <Text>Bhvaya Wade</Text>
         </VStack>
       </Stack>
     </Box>
